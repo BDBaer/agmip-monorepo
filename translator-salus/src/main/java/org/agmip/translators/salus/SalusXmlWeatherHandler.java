12100ed714db18a370c99af32877450d56b64ef5
Brian Baer <baerb@msu.edu>
2022-05-03 17:04:21 -0400

    End of Day 1
    End of 3 May 2022 Workshop
    
    Created an ObjectMapper for the daily weather data in
    SalusXmlWeatherHandler.java file.

--- translator-salus/src/main/java/org/agmip/translators/salus/SalusXmlWeatherHandler.java
+++ translator-salus/src/main/java/org/agmip/translators/salus/SalusXmlWeatherHandler.java
@@ -1,9 +1,16 @@
 package org.agmip.translators.salus;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import java.io.FileWriter;
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class SalusXmlWeatherHandler extends DefaultHandler {
@@ -30,7 +37,33 @@ public class SalusXmlWeatherHandler extends DefaultHandler {
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
-        //System.out.println(_sb.toString());
+        if (qName == "Weather") {
+            System.out.println("In New Weather Station");
+            String[] colNames = "Year,DOY,SRAD,Tmax,Tmin,Rain,DewP,Wind,PAR".split(",");
+
+            //String json = "{ \"f1\" : \"v1\" } ";
+            ObjectMapper objectMapper = new ObjectMapper();
+            ArrayNode jsonNode = objectMapper.createArrayNode();
+
+            int i, j;
+            // Convert data blob to array of strings
+            String[] days = _sb.toString().split(System.lineSeparator());
+            // Loop through the days, only process non-zero length strings
+            for (i = 0; i < days.length; i++) {
+                if (days[i].length() > 6) {
+                    String[] wthValues = days[i].split(",");
+                    System.out.println(String.valueOf(colNames.length) + "  " + String.valueOf(wthValues.length));
+                    ObjectNode wthColValue = objectMapper.createObjectNode();
+                    // the split ignores trailing blank values so use the shortest of the header or data arrays
+                    for (j = 0; j < Math.min(colNames.length, wthValues.length); j++) {
+                        wthColValue.put(colNames[j], wthValues[j]);
+                    }
+                    jsonNode.add(wthColValue);
+                }
+            }
+            String outFN = "/Users/baerb/OneDriveMSU/DataHarmonization/2022-05-02_Workshop/junk.json";
+//            System.out.println(jsonNode.toPrettyString());
+        }
     }
 }
 
